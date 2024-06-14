a26685bdba1e2510764b0159d7456e37fc6fac24
MathewAnto <mathewanto.19@gmail.com>
2024-06-14 20:00:29 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,107 @@
+// import logo from './logo.svg';
+import "./App.css";
+import { Home } from "./components/Home";
+import vid1 from './img/vid1.png'
+import dp1 from './img/dp1.png'
+import vid2 from './img/vid2.png'
+import dp2 from './img/dp2.png'
+import vid3 from './img/vid3.png'
+import dp3 from './img/dp3.png'
+import vid4 from './img/vid4.png'
+import dp4 from './img/dp4.png'
+import vid5 from './img/vid5.png'
+import dp5 from './img/dp5.png'
+import vid6 from './img/vid6.png'
+import dp6 from './img/dp6.png'
+import vid7 from './img/vid7.png'
+import dp7 from './img/dp7.png'
+
+function App() {
+  // thumbnail
+  // dp
+  // Code Tetris: JavaScript Tutorial for Beginners
+  // freeCodeCamp.org
+  // 375K views
+  // 3 years ago
+
+  const data = [
+    {
+      thumbnail: vid1,
+      vidLink: "https://www.youtube.com/embed/rAUn1Lom6dw?si=uk2EHdqtgO7BRRoP?autoplay=1",
+      dp: dp1,
+      title: "Code Tetris: JavaScript Tutorial for Beginners",
+      site: "freeCodeCamp.org",
+      siteLink: "https://www.youtube.com/@freecodecamp",
+      views: "375K views",
+      uptime: "3 years ago"
+    },
+    {
+      thumbnail: vid2,
+      vidLink: "https://www.youtube.com/embed/J5j95RUSLd8?si=Ifuf8FnfxgbU7hSm?autoplay=1",
+      dp: dp2,
+      title: "2024 Total Solar Eclipse: Through the Eyes of NASA (Telescope Feed)",
+      site: "NASA",
+      siteLink: "https://www.youtube.com/@NASA",
+      views: "135K views",
+      uptime: "1 month ago"
+    },
+    {
+      thumbnail: vid3,
+      vidLink: "https://www.youtube.com/embed/w4N22lhkEPg?si=ffmYPpoRB-pjoTnT?autoplay=1",
+      dp: dp3,
+      title: "Be Notorious End Credit Version Lyric Video | Bheeshmaparvam |...",
+      site: "123Musix",
+      siteLink: "https://www.youtube.com/@123Musix",
+      views: "8.2M views",
+      uptime: "2 years ago"
+    },
+    {
+      thumbnail: vid4,
+      vidLink: "https://www.youtube.com/embed/EA_mzs4ICwo?si=U76HaHt6avpIQuOj?autoplay=1",
+      dp: dp4,
+      title: "City of the Sun - W.16th St. | Sofar Washington D.C.",
+      site: "Sofar Sounds",
+      siteLink: "https://www.youtube.com/@Sofarsounds",
+      views: "4.3M views",
+      uptime: "8 years ago"
+    },
+    {
+      thumbnail: vid5,
+      vidLink: "https://www.youtube.com/embed/1-8i8Q6Si3w?si=21Y5ol97m-VZydvI?autoplay=1",
+      dp: dp5,
+      title: "1 month with the Jimny has changed me | Malayalam Review | Content...",
+      site: "Content With Cars",
+      siteLink: "https://www.youtube.com/@ContentWithCars",
+      views: "144K views",
+      uptime: "4 months ago"
+    },
+    {
+      thumbnail: vid6,
+      vidLink: "https://www.youtube.com/embed/fy3TqHU7DsU?si=tT0eAFMkvaOn8S3k?autoplay=1",
+      dp: dp6,
+      title: "Namma Stories - The South Anthem | NJ, Arivu, SIRI & Hanumankind |...",
+      site: "Netflix India",
+      siteLink: "https://www.youtube.com/@NetflixIndiaOfficial",
+      views: "9.3M views",
+      uptime: "2 years ago"
+    },
+    {
+      thumbnail: vid7,
+      vidLink: "https://www.youtube.com/embed/EW4pNzAbVao?si=vlMaHS7ZeFaSOvag?autoplay=1",
+      dp: dp7,
+      title: "KSHMR & Lost Stories - Bombay Dreams [feat. Kavita Seth] (Official Music Video)",
+      site: "KSHMR",
+      siteLink: "https://www.youtube.com/channel/UCFMjkrMT7Gvg84v0av-DIwA",
+      views: "40M views",
+      uptime: "4 years ago"
+    }
+  ]
+
+  return (
+    <div className="App">
+      <Home data={data}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,339 @@
+import React, { useState, useRef } from "react";
+import "./Home.css";
+import logo from "../img/logo.PNG";
+import search from "../img/search.PNG";
+import mic from "../img/mic.png";
+import video from "../img/video.PNG";
+import grid from "../img/grid.PNG";
+import bell from "../img/bell.PNG";
+import profilePic from "../img/profile-pic.png";
+import { IoReorderThreeOutline } from "react-icons/io5";
+
+import home from "../img/home.PNG";
+import explore from "../img/explore.PNG";
+import subscription from "../img/subscription.PNG";
+import library from "../img/library.PNG";
+import history from "../img/history.PNG";
+import yourVideo from "../img/your-video.PNG";
+import watchLater from "../img/watch-later.PNG";
+import likedVideo from "../img/liked video.PNG";
+import showMore from "../img/show more.PNG";
+import showArrow from "../img/show arrow.PNG";
+
+import rArrow from "../img/rArrow.PNG";
+import lArrow from "../img/lArrow.PNG";
+
+export const Home = ({ data }) => {
+ 
+  const [showFirstContent, setShowFirstContent] = useState(true);
+  const [isToggled, setToggled] = useState(false);
+
+  const toggleContent = () => {
+    setShowFirstContent(!showFirstContent);
+    setToggled(!isToggled);
+  };
+
+  const scrollContainer = useRef(null);
+
+  const scrollLeft = () => {
+    if (scrollContainer.current) {
+      scrollContainer.current.scrollBy({ left: -200, behavior: "smooth" });
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollContainer.current) {
+      scrollContainer.current.scrollBy({ left: 200, behavior: "smooth" });
+    }
+  };
+
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
+
+  const filterOptions = [
+    "all",
+    "CSS",
+    "web development",
+    "python",
+    "entertainment",
+    "marvel",
+    "javascript",
+    "artificial intelligence",
+    "machine learning",
+    "trending",
+    // Add other filter options here
+  ];
+
+  return (
+    <>
+      <div className="navbar">
+        <div className="toggle-btn" onClick={toggleContent}>
+          {/* <a onClick={toggleContent}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </a> */}
+          <IoReorderThreeOutline id="toggleBtn"/>
+        </div>
+        <a href="https://www.youtube.com/" target="_blank">
+          <img src={logo} className="logo" alt="Youtube Logo" />
+        </a>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="search" />
+          <button className="search-btn">
+            <img src={search} alt="Search Logo" />
+          </button>
+          <div className="search-mic">
+            <img src={mic} className="icon" alt="" id="mic" />
+          </div>
+        </div>
+        <div className="user-options">
+          <img src={video} className="icon" alt="" />
+          <img src={grid} className="icon" alt="" />
+          <img src={bell} className="icon" alt="" />
+          <div className="user-dp">
+            <img src={profilePic} alt="" />
+          </div>
+        </div>
+      </div>
+
+      {showFirstContent ? (
+
+      <div className="side-bar-wide">
+        <a href="#" className="links active">
+          <img src={home} alt="" />
+          home
+        </a>
+        <a href="#" className="links">
+          <img src={explore} alt="" />
+          explore
+        </a>
+        <a href="#" className="links">
+          <img src={subscription} alt="" />
+          subscriptions
+        </a>
+
+        <hr className="seperator" />
+
+        <a
+          href="#"
+          className="links"
+          style={{ fontSize: "1.2rem", fontWeight: "700" }}
+        >
+          You
+          <img src={showArrow} alt="" />
+        </a>
+        <a href="#" className="links">
+          <img src={library} alt="" />
+          library
+        </a>
+        <a href="#" className="links">
+          <img src={history} alt="" />
+          history
+        </a>
+        <a href="#" className="links">
+          <img src={yourVideo} alt="" />
+          your videos
+        </a>
+        <a href="#" className="links">
+          <img src={watchLater} alt="" />
+          watch later
+        </a>
+        <a href="#" className="links">
+          <img src={likedVideo} alt="" />
+          liked videos
+        </a>
+
+        <hr className="seperator" />
+
+        <p
+          className="subscrip"
+          style={{ fontSize: "1.2rem", fontWeight: "700" }}
+        >
+          Subscriptions
+        </p>
+        <a href="#" className="links">
+          <img src={library} alt="" />
+          library
+        </a>
+        <a href="#" className="links">
+          <img src={history} alt="" />
+          history
+        </a>
+        <a href="#" className="links">
+          <img src={yourVideo} alt="" />
+          your videos
+        </a>
+        <a href="#" className="links">
+          <img src={watchLater} alt="" />
+          watch later
+        </a>
+        <a href="#" className="links">
+          <img src={likedVideo} alt="" />
+          liked videos
+        </a>
+        <a href="#" className="links">
+          <img src={showMore} alt="" />
+          show more
+        </a>
+
+        <hr className="seperator" />
+
+        <p
+          className="subscrip"
+          style={{ fontSize: "1.2rem", fontWeight: "700" }}
+        >
+          Explore
+        </p>
+        <a href="#" className="links">
+          <img src={library} alt="" />
+          trending
+        </a>
+        <a href="#" className="links">
+          <img src={history} alt="" />
+          shopping
+        </a>
+        <a href="#" className="links">
+          <img src={yourVideo} alt="" />
+          music
+        </a>
+        <a href="#" className="links">
+          <img src={watchLater} alt="" />
+          movies
+        </a>
+        <a href="#" className="links">
+          <img src={likedVideo} alt="" />
+          live
+        </a>
+        <a href="#" className="links">
+          <img src={library} alt="" />
+          gaming
+        </a>
+        <a href="#" className="links">
+          <img src={history} alt="" />
+          news
+        </a>
+        <a href="#" className="links">
+          <img src={yourVideo} alt="" />
+          sports
+        </a>
+        <a href="#" className="links">
+          <img src={watchLater} alt="" />
+          courses
+        </a>
+        <a href="#" className="links">
+          <img src={likedVideo} alt="" />
+          podcasts
+        </a>
+        <a href="#" className="links">
+          <img src={showMore} alt="" />
+          show more
+        </a>
+
+        <hr className="seperator" />
+
+        <a href="#" className="links">
+          <img src={library} alt="" />
+          settings
+        </a>
+        <a href="#" className="links">
+          <img src={history} alt="" />
+          report history
+        </a>
+        <a href="#" className="links">
+          <img src={yourVideo} alt="" />
+          help
+        </a>
+        <a href="#" className="links">
+          <img src={watchLater} alt="" />
+          send feedback
+        </a>
+      </div>
+
+) : (
+      <div className="side-bar-small">
+
+        <a href="#" className="side-links">
+          <img src={home} alt="" />
+          <span>home</span>
+        </a>
+        <a href="#" className="side-links">
+          <img src={explore} alt="" />
+          <span>explore</span>
+        </a>
+        <a href="#" className="side-links">
+          <img src={subscription} alt="" />
+          <span>subscriptions</span>
+        </a>
+        <a href="#" className="side-links">
+          <img src={library} alt="" />
+          <span>library</span>
+        </a>
+
+      </div>
+
+)}
+
+      <div className={isToggled ? 'filters-wide' : 'filters'}>
+        <button className="arrow" onClick={scrollLeft}>
+          <img src={lArrow} alt="" />
+        </button>
+
+        <div className="filter-opts" ref={scrollContainer}>
+          
+          <div className="filter-options-container">
+            {filterOptions.map((option) => (
+              <button
+                // key={option}
+                className={`filter-options ${
+                  activeButton === option ? "active" : ""
+                }`}
+                onClick={() => handleButtonClick(option)}
+              >
+                {option}
+              </button>
+            ))}
+          </div>
+
+        </div>
+
+        <button className="arrow" onClick={scrollRight}>
+          <img src={rArrow} alt="" />
+        </button>
+      </div>
+
+      <div className={isToggled ? 'video-container-wide' : 'video-container'}>
+        {data.map((data) => (
+          <div className="videoDiv">
+            <iframe
+              src={data.vidLink}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              className={isToggled ? 'video-wide' : 'video'}
+            ></iframe>
+            <div className="content">
+              <a href={data.siteLink} target="_blank"><img src={data.dp} className="channel-icon" alt="" /></a>
+              <div className="info">
+                <a href={data.vidLink} target="_blank">
+                  <h4 className="title">{data.title}</h4>
+                </a>
+                <a href={data.siteLink} target="_blank">
+                  <p className="channel-name">{data.site}</p>
+                </a>
+                <p className="viewsAndUptime">
+                  {data.views} <b>.</b> {data.uptime}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
